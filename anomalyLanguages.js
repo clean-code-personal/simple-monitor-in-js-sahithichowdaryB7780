efc224365aa12555b9a307631d13c90f187b47e6
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-16 13:20:26 +0530

    Update anomalyLanguages.js

--- anomalyLanguages.js
+++ anomalyLanguages.js
@@ -1,15 +1,16 @@
 const anomalyMessages = {
-    LOW_TEMPERATURE_BREACH: { en: 'Low temperature breach', ge: 'Niedrige Temperaturüberschreitung' },
+    LOW_TEMPERATURE_BREACH: { en: 'Low temperature breach', ge: 'Niedrige TemperaturÃ¼berschreitung' },
     LOW_TEMPERATURE_WARNING: { en: 'Low temperature warning', ge: 'Niedrige Temperaturwarnung' },
     HIGH_TEMPERATURE_WARNING: { en: 'High temperature warning', ge: 'Hohe Temperaturwarnung' },
-    HIGH_TEMPERATURE_BREACH: { en: 'High temperature breach', ge: 'Hohe Temperaturüberschreitung' },
-    LOW_SOC_BREACH: { en: 'Low SoC breach', ge: 'Niedriger SoC-Überschreitung' },
+    HIGH_TEMPERATURE_BREACH: { en: 'High temperature breach', ge: 'Hohe TemperaturÃ¼berschreitung' },
+    LOW_SOC_BREACH: { en: 'Low SoC breach', ge: 'Niedriger SoC-Ãœberschreitung' },
     LOW_SOC_WARNING: { en: 'Low SoC warning', ge: 'Niedriger SoC-Warnung' },
     HIGH_SOC_WARNING: { en: 'High SoC warning', ge: 'Hohe SoC-Warnung' },
-    HIGH_SOC_BREACH: { en: 'High SoC breach', ge: 'Hohe SoC-Überschreitung' },
+    HIGH_SOC_BREACH: { en: 'High SoC breach', ge: 'Hohe SoC-Ãœberschreitung' },
     LOW_CHARGE_RATE_WARNING: { en: 'Low charge rate warning', ge: 'Niedrige Ladegeschwindigkeitswarnung' },
     HIGH_CHARGE_RATE_WARNING: { en: 'High charge rate warning', ge: 'Hohe Ladegeschwindigkeitswarnung' },
-    NORMAL: { en: 'Normal', ge: 'Normale' }
+    NORMAL: { en: 'Normal', ge: 'Normale' },
+    OUT_OF_RANGE: { en: 'Out of range', ge: 'Auberhalb des Bereichs' }
 
 };
 
